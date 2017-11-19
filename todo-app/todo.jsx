0af4618ae5abea3446f57fca67e687f0d0bd6a3d
kazuya-seki <track613armory@gmail.com>
2017-11-19 10:22:38 +0900

    コンポーネントを抽出するリファクタリング

--- todo-app/todo.jsx
+++ todo-app/todo.jsx
@@ -97,6 +97,38 @@ const FilterLink = ({
   )
 }
 
+const Todo = ({
+  onClick,
+  completed,
+  text
+}) => (
+  <li key={todo.id}
+    onClick={onClick}
+    style={{
+      textDecoration:
+        completed ?
+          'line-through' :
+          'none'
+    }}>
+    {text}
+  </li>
+)
+
+const TodoList = ({
+  todos,
+  onTodoClick
+}) => (
+  <ul>
+    {todos.map(todo => 
+      <Todo
+        key={todo.id}
+        {...todo}
+        onClick={() => onTodoClick(todo.id)}
+      />
+    )}
+  </ul>
+)
+
 class TodoApp extends Component {
   render() {
     const {
@@ -122,25 +154,14 @@ class TodoApp extends Component {
         }} >
           Add Todo
         </button>
-        <ul>
-          {visibleTodos.map(todo => 
-            <li key={todo.id}
-              onClick={() => {
-                store.dispatch({
-                  type: 'TOGGLE_TODO',
-                  id: todo.id
-                });
-              }}
-              style={{
-                textDecoration:
-                  todo.completed ?
-                    'line-through' :
-                    'none'
-              }}>
-              {todo.text}
-            </li>
-          )}
-        </ul>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={id =>
+            store.dispatch({
+              type: 'TOGGLE_TODO',
+              id
+            })
+          } />
         <p>
           Show:
           {' '}
