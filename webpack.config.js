709c09730744522978877124facb8e8ec28550a8
kazuya-seki <track613armory@gmail.com>
2018-04-07 22:42:34 +0900

    added

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,25 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: {
-      counter: __dirname + "/first-app/counter.jsx",
-      todo: __dirname + "/todo-app/todo.jsx"
+    counter: __dirname + '/first-app/counter.jsx',
+    todo: __dirname + '/todo-app/todo.jsx'
   },
   output: {
-      path: __dirname + "/dist",
-      filename: "[name].js"
+    path: __dirname + '/dist',
+    filename: '[name].js'
   },
   resolve: {
-    extensions: [ ".jsx", ".js" ]
+    extensions: ['.jsx', '.js'],
+    alias: {
+      react: path.resolve('./node_modules/react')
+    }
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   module: {
     loaders: [
-        { test: /\.jsx?$/, exclude: /node_modules/, loaders: 'babel-loader' }
+      { test: /\.jsx?$/, exclude: /node_modules/, loaders: 'babel-loader' }
     ]
   },
   plugins: []
